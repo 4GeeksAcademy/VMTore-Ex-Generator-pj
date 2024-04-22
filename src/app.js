77c5fa0b959cb626406add20b58db1310f3f94e2
VMTore <151078881+VMTore@users.noreply.github.com>
2024-04-22 19:38:39 +0000

    Corrijo links, añado funcion y html

--- src/app.js
+++ src/app.js
@@ -1,11 +1,35 @@
 /* eslint-disable */
-import "bootstrap";
-import "./style.css";
+alert("Enlazado");
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
-};
+
+  //Arrays
+
+  const who = ['The dog ', 'My grandma ', 'The mailman ', 'My bird '];
+  const action = ['ate ', 'peed ', 'crushed ', 'broke '];
+  const what = ['my homework ', 'my phone ', 'the car '];
+  const when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+
+  //Función
+
+  function excuseGenerator() {
+    
+    let randomWho = Math.floor(Math.random() * who.length);
+    let randomAction = Math.floor(Math.random() * action.length);
+    let randomWhat = Math.floor(Math.random() * what.length);
+    let randomWhen = Math.floor(Math.random() * when.length);
+
+    const excuse = who [randomWho] + action [randomAction] + what [randomWhat] + when [randomWhen];
+
+    console.log(excuse);
+
+    excuse.join( );
+
+  }
+
+    excuseGenerator()
+}
+
+document.querySelector("#excuse").textContent = excuse
