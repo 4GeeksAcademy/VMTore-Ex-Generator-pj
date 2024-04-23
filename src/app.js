ebdbb6c88c41401dac04a0ee1ea53c4e0d0b8ccc
VMTore <151078881+VMTore@users.noreply.github.com>
2024-04-23 16:57:08 +0000

    quitando join

--- src/app.js
+++ src/app.js
@@ -1,35 +1,43 @@
 /* eslint-disable */
-alert("Enlazado");
+import "bootstrap";
+import "./style.css";
 
+import "./assets/img/rigo-baby.jpg";
+import "./assets/img/4geeks.ico";
+//alert("Enlazado");
 
 window.onload = function() {
   //write your code here
 
   //Arrays
 
-  const who = ['The dog ', 'My grandma ', 'The mailman ', 'My bird '];
-  const action = ['ate ', 'peed ', 'crushed ', 'broke '];
-  const what = ['my homework ', 'my phone ', 'the car '];
-  const when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+  const who = ["The dog ", "My grandma ", "The mailman ", "My bird "];
+  const action = ["ate ", "peed ", "crushed ", "broke "];
+  const what = ["my homework ", "my phone ", "the car "];
+  const when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ];
 
   //Función
 
   function excuseGenerator() {
-    
-    let randomWho = Math.floor(Math.random() * who.length);
+    let randomWho = [Math.floor(Math.random() * who.length)];
     let randomAction = Math.floor(Math.random() * action.length);
     let randomWhat = Math.floor(Math.random() * what.length);
     let randomWhen = Math.floor(Math.random() * when.length);
 
-    const excuse = who [randomWho] + action [randomAction] + what [randomWhat] + when [randomWhen];
-
-    console.log(excuse);
-
-    excuse.join( );
-
+    const excuse =
+      who[randomWho] +
+      action[randomAction] +
+      what[randomWhat] +
+      when[randomWhen];
   }
 
-    excuseGenerator()
-}
+  document.querySelector("#excuses").textContent = excuseGenerator();
+};
 
-document.querySelector("#excuse").textContent = excuse
+excuseGenerator();
