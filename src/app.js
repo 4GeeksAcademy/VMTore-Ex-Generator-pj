11e79cf69600cd96d9f0bf92176e641b8a1815be
VMTore <151078881+VMTore@users.noreply.github.com>
2024-04-25 19:14:17 +0000

    añadidos retoques css

--- src/app.js
+++ src/app.js
@@ -1,10 +1,4 @@
 /* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-//alert("Enlazado");
 
 window.onload = function() {
   //write your code here
